6c1ab5c2073f1eb1cce2c4c6e805910d4d5bdb65
Val Kasabrukhau <vkasabrukhau@gmail.com>
2023-03-26 14:23:23 -0400

    this is the commit without the key

--- airline/settings.py
+++ airline/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+dz6#z*+v7!izk^w6=n7)2urqodr%3*atv1dcr!!+o39(qfi8^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
